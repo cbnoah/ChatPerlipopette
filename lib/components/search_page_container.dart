5cbf550b93f59d41fe401279310adf18e51264f7
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-03-09 12:28:51 +0100

    feat: add SearchPageContainer widget for search results display

---
+++ lib/components/search_page_container.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+class SearchPageContainer extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String description;
+  final String imageUrl;
+  final List<String> tags;
+  final VoidCallback? onPressed;
+
+  const SearchPageContainer({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.description,
+    required this.imageUrl,
+    required this.tags,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 400,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFFF48C25).withAlpha(25),
+            blurRadius: 4.0,
+            offset: const Offset(0, -2),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                // Image with rounded top corners
+                ClipRRect(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+                  clipBehavior: Clip.hardEdge,
+                  child: AspectRatio(
+                    aspectRatio: 2,
+                    child: Image.network(
+                      alignment: FractionalOffset.topCenter,
+                      fit: BoxFit.cover,
+                      imageUrl,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Texts (Title, Subtitle, Description)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Title
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontFamily: "Plus Jakarta Sans",
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 2),
+                      // Subtitle
+                      Text(
+                        subtitle.toUpperCase(),
+                        style: const TextStyle(
+                          fontFamily: "Plus Jakarta Sans",
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFFF48C25),
+                        ),
+                      ),
+                      const SizedBox(height: 14),
+                      // Description
+                      Text(
+                        description,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontFamily: "Plus Jakarta Sans",
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 6,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            // Tags and Button
+            Padding(
+              padding: tags.isEmpty ? const EdgeInsets.only(right: 15.0) : EdgeInsetsGeometry.all(0),
+              child: Row(
+                mainAxisAlignment: tags.isEmpty ? MainAxisAlignment.end : MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Tags
+                  // Tag 1
+                  Row(
+                    spacing: 8,
+                    children: [
+                      ?tags.isEmpty
+                          ? null
+                          : Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8,
+                                vertical: 6,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Color(0xFFF48C25).withAlpha(25),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Text(
+                                tags[0].toUpperCase(),
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color(0xFFF48C25),
+                                  fontFamily: "Plus Jakarta Sans",
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                      // Tag 2
+                      ?tags.length < 2
+                          ? null
+                          : Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8,
+                                vertical: 6,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Color(0xFFF48C25).withAlpha(25),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Text(
+                                tags[1].toUpperCase(),
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Color(0xFFF48C25),
+                                  fontFamily: "Plus Jakarta Sans",
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                    ],
+                  ),
+                  // Button "Voir plus" with container for BoxShadow
+                  Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.white.withAlpha(20),
+                          blurRadius: 4.0,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFF48C25),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                      child: const Text(
+                        "Voir plus",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Plus Jakarta Sans",
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
