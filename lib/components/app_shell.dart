ec7bc9e32f94e7e4a68dfde9f1ad7ff9054be18a
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-03-16 10:12:35 +0100

    feat: add AppShell with bottom navigation and home/search pages

---
+++ lib/components/app_shell.dart
@@ -0,0 +1,38 @@
+import 'package:chatperlipopette/pages/search.dart';
+import 'package:flutter/material.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _index = 0;
+
+  final _pages = const <Widget>[
+    HomePage(),
+    Search(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _index,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (i) => setState(() => _index = i),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Accueil'),
+          BottomNavigationBarItem(icon: Icon(Icons.pets), label: 'Chats'),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget { const HomePage({super.key}); @override Widget build(_) => const Center(child: Text('Home')); }
