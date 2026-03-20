ec63f5e29eecac2ab9e9e0251ab35c7877702201
cnoah <noah.charrinbourrat@ynov.com>
2026-03-20 22:55:06 +0100

    feat: go router has been added to prepare for potential deep link integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,49 @@
 import 'package:chatperlipopette/components/app_shell.dart';
-import 'package:chatperlipopette/pages/search.dart';
-import 'package:chatperlipopette/themes/light_theme.dart';
 import 'package:chatperlipopette/pages/catsInfos.dart';
-import 'package:chatperlipopette/pages/home.dart';
+import 'package:chatperlipopette/themes/light_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   runApp(const MyApp());
 }
+
+final router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => AppShell()),
+    GoRoute(
+      path: '/breed/:id',
+      builder: (context, state) {
+        final id = state.pathParameters['id']!;
+        return CatsInfos(id: id);
+      },
+    ),
+    // W.I.P, Need to be implemented for the map, the same way as the breed
+    /*
+    GoRoute(
+      path: '/breed/:id/map',
+      builder: (context, state) {
+        final id = state.pathParameters['id']!;
+        return CatMap(id: id);
+      },
+    ),
+     */
+  ],
+  initialLocation: '/'
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
-      initialRoute: '/',
-      routes: {
-        // Home page with bottom navigation bar (need to be implemented)
-        '/': (context) => AppShell(),
-        '/search': (context) => Search(),
-      },
+      routerConfig: router,
     );
   }
 }
