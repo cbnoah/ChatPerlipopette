6ec858fcf13d5ea2346d03a81b34b746914be252
cnoah <noah.charrinbourrat@ynov.com>
2026-03-23 22:05:02 +0100

    feat: convert CalmCategorySection to StatefulWidget and fetch random cats from API

--- lib/components/calm_category_section.dart
+++ lib/components/calm_category_section.dart
@@ -1,29 +1,68 @@
 import 'package:flutter/material.dart';
+import '../utils/api_scrapper.dart';
+import '../utils/cat.dart';
 import 'cat_horizontal_card.dart';
 
-class CalmCategorySection extends StatelessWidget {
+class CalmCategorySection extends StatefulWidget {
   final BuildContext context;
 
-  const CalmCategorySection({required this.context});
+  const CalmCategorySection({super.key, required this.context});
+
+  @override
+  State<CalmCategorySection> createState() => _CalmCategorySectionState();
+}
+
+class _CalmCategorySectionState extends State<CalmCategorySection> {
+  late final Future<List<Cat>> randomCats;
+
+  @override
+  void initState() {
+    super.initState();
+    randomCats = _fetchRandomCats();
+  }
+
+  Future<List<Cat>> _fetchRandomCats() async {
+    final cats = await fetchCatsList();
+    final shuffled = List<Cat>.from(cats)..shuffle();
+    return shuffled.take(2).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CatHorizontalCard(
-          name: 'Scottish Fold',
-          description: 'Un tempérament paisible et indépendant.',
-          imagePath: 'assets/images/Cat2.png',
-          context: context,
-        ),
-        const SizedBox(height: 10),
-        CatHorizontalCard(
-          name: 'Persan',
-          description: 'La quintessence du chat de salon',
-          imagePath: 'assets/images/Cat3.png',
-          context: context,
-        ),
-      ],
+    return FutureBuilder<List<Cat>>(
+      future: randomCats,
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (asyncSnapshot.hasError) {
+          return const Center(child: Text('Erreur de chargement'));
+        }
+
+        final cats = asyncSnapshot.data ?? const <Cat>[];
+        if (cats.isEmpty) {
+          return const Center(child: Text('Aucun chat trouve'));
+        }
+
+        return ListView.separated(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (BuildContext context, int index) {
+            final cat = cats[index];
+            return CatHorizontalCard(
+              name: cat.name,
+              description: cat.description,
+              imagePath: "https://cdn2.thecatapi.com/images/${cat.imageRefId}.jpg",
+              context: context,
+              id: cat.id,
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const SizedBox(height: 10),
+          itemCount: cats.length,
+        );
+      },
     );
   }
 }
